698b4d07d0458de387794171ac8b31a398562ce5
PhilippRa <Philipp@Philipps-Mac.local>
2014-02-03 12:05:59 +0100

    v 1.2.1
    Changed

--- backup-mysql.cfg.php
+++ backup-mysql.cfg.php
@@ -4,8 +4,6 @@
 	General
 ***************************************** */
 define("METHOD", ""); // The backup method ("email" or "ftp")
-// Not supported yet:
-// define("DELTIME", 0); // Delete files (on FTP server) which are older than this time (in seconds)
 
 /* *****************************************
 	FTP Settings
