2816413e7269f4d7b4ff0b51a8ac730104c5243c
Martin Bohal <mb@mbohal.cz>
2014-06-19 10:02:18 +0200

    whitespace + removed end php tag

--- backup-files.cfg.php
+++ backup-files.cfg.php
@@ -34,7 +34,5 @@ $BACKUPDIRS = array(
 // Some random keys for GET requests
 // Example:
 // backup-files.php?KEY_GET1=randomStuff&KEY_GET2=otherStuff
-define("KEY_GET1", "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad"); 
-define("KEY_GET2", "ab5df625bc76dbd4e163bed2dd888df828f90159bb93556525c31821b6541d46"); 
-
-?>
+define("KEY_GET1", "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad");
+define("KEY_GET2", "ab5df625bc76dbd4e163bed2dd888df828f90159bb93556525c31821b6541d46");
